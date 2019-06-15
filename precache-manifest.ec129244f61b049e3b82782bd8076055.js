f21dc8fd58b816265651de838fbff2e339b60e0b
shoupingliang <shoupingliang@gmail.com>
2019-06-15 11:01:33 +0200

    Updates

---
+++ precache-manifest.ec129244f61b049e3b82782bd8076055.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07ce2fd7f3376acde402a503974c31a",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "6d83ed75c26cf87c5c93",
+    "url": "/project/static/css/main.1e740fbd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/project/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6d83ed75c26cf87c5c93",
+    "url": "/project/static/js/main.87aa04a9.chunk.js"
+  },
+  {
+    "revision": "369bc2d987da7beb9860",
+    "url": "/project/static/js/runtime~main.4b9e7fa5.js"
+  }
+]);
